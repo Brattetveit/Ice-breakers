f0c7bce90be6631ba5759ba8145e09769441ef45
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-01-29 22:01:20 +0100

    server running

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const port = 3500;
+
 const express = require("express");
+const colors = require("colors");
+
+const app = express();
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`.underline.cyan)
+);
+
+app.get("", (req, res) => {
+  res.send({ message: "Hello from server!" });
+});
