4af58caed6c9004d45760cab09f1f7b73e34f088
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-01-29 22:07:12 +0100

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const port = 3500;
-
 const express = require("express");
 const colors = require("colors");
 
 const app = express();
+const port = process.env.PORT || 3500;
+
+require("dotenv").config();
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`.underline.cyan)
