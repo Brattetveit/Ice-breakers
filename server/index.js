112b163acfc5e96d548d42b7f79cda905386f966
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-02-03 15:23:54 +0100

    feat: 🎸 mongodb connection
    ✅ Closes: #1

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const colors = require("colors");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3500;
 
-require("dotenv").config();
+app.use(cors());
 
-app.listen(port, () =>
-  console.log(`Server running on http://localhost:${port}`.underline.cyan),
-);
-
-app.get("", (req, res) => {
-  res.send({ message: "Hello from server!" });
+app.get("/", (req, res) => {
+  res.status(234).json({ message: "Hello World" });
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected".underline.green);
+    app.listen(port, () =>
+      console.log(`Server running on http://localhost:${port}`.underline.cyan)
+    );
+  })
+  .catch((error) => console.log(error));
