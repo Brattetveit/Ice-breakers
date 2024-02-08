b71f5531fcbbc153205ac19704275a3908497eec
Olav Brattetveit <olavhbr@stud.ntnu.no>
2024-02-08 13:27:11 +0100

    Made login and register pages + creation of label component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<h1>Welcome to the app!</h1>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
