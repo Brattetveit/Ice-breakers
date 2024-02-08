f9868382befca1342b03076f62d07b584993129d
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-02-08 14:25:15 +0100

    category based carousels

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,16 +7,13 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { useGetIcebreakers } from "@/hooks/useGetIcebreakers";
 import { type Icebreaker } from "@/types";
-import { useEffect } from "react";
 
 const CAROUSEL_COLORS = ["#A3CEF1", "#ADE8F4", "#6096BA"];
 
-export const Home = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { isLoading, icebreakers, getIcebreakers } = useGetIcebreakers();
+const CATEGORIES = ["All", "Funny", "Serious", "Random"];
 
+export const Home = () => {
   const icebreakersTest: Icebreaker[] = [
     {
       name: "Icebreaker",
@@ -47,50 +44,43 @@ export const Home = () => {
     },
   ];
 
-  useEffect(() => {
-    getIcebreakers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className="flex min-h-screen w-full flex-col items-center gap-14 p-4">
       <div className="flex w-1/2 justify-center rounded bg-[#507DBC] p-6 text-white">
         <H1>Ice Breakers</H1>
       </div>
-      <div className="sm:w-1/3 lg:w-2/3">
-        {isLoading ? (
-          "Loading..."
-        ) : (
-          <Carousel
-            opts={{
-              loop: true,
-              startIndex: 1,
-            }}
-          >
-            <CarouselContent>
-              {Array.from({ length: icebreakersTest.length }).map((_, idx) => (
-                <CarouselItem key={idx} className="md:basis-1/2 lg:basis-1/3">
-                  <div className="p-1">
-                    <Card
-                      style={{
-                        backgroundColor:
-                          CAROUSEL_COLORS[idx % CAROUSEL_COLORS.length],
-                      }}
-                    >
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-3xl font-semibold text-white">
-                          {icebreakersTest[idx].name}
-                        </span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        )}
+      <div className="flex w-5/6 flex-col gap-6">
+        {CATEGORIES.map((category, idx) => {
+          return (
+            <div key={idx}>
+              <h2>{category}</h2>
+              <Carousel>
+                <CarouselContent>
+                  {icebreakersTest.map((icebreaker, idx) => (
+                    <CarouselItem key={idx} className="basis-1/5">
+                      <div className="">
+                        <Card
+                          style={{
+                            backgroundColor:
+                              CAROUSEL_COLORS[idx % CAROUSEL_COLORS.length],
+                          }}
+                        >
+                          <CardContent className="flex aspect-video items-center justify-center p-6">
+                            <span className="text-3xl font-semibold text-white">
+                              {icebreaker.name}
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
