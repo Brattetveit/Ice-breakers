8ff42f6e6a32084a0000068e29bba90db6ac1a0e
Olav Brattetveit <olavhbr@stud.ntnu.no>
2024-02-08 15:40:23 +0100

    Styled login and register page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -13,32 +13,32 @@ import { Label } from "@/components/ui/label";
 export const Login = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <Card className="w-[350px]">
+      <Card className="w-1/2 h-3/5 bg-[#6096BA]">
         <CardHeader>
-          <CardTitle>Log in</CardTitle>
+          <CardTitle className="place-self-center text-4xl mt-3">Log in</CardTitle>
         </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
+        <CardContent className="m-5">
+          <form className="mt-5">
+            <div className="grid w-full gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="username">Username</Label>
-                <Input id="username" placeholder="Username" />
+                <Input id="username" placeholder="Username" className="bg-[#d9d9d9]" />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="Password" />
+                <Label htmlFor="password" className="mt-5">Password</Label>
+                <Input id="password" type="password" placeholder="Password" className="bg-[#d9d9d9]" />
               </div>
             </div>
           </form>
         </CardContent>
-        <CardFooter className="flex flex-col items-center">
-          <p className="mt-2 text-sm text-gray-600">
+        <CardFooter className="flex flex-col items-center m-5">
+          <p className="text-sm text-gray-600">
             Not a user?{" "}
-            <Link to="/register" className="text-blue-500">
+            <Link to="/register" className="text-white">
               Register here
             </Link>
           </p>
-          <div className="mt-4 flex w-full justify-between">
+          <div className="mt-4 flex w-4/5 justify-between">
             <Button variant="outline">Cancel</Button>
             <Button>Log in</Button>
           </div>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -13,32 +13,32 @@ import { Label } from "@/components/ui/label";
 export const Register = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <Card className="w-[350px]">
+      <Card className="w-1/2 h-3/5 bg-[#6096BA]">
         <CardHeader>
-          <CardTitle>Register user</CardTitle>
+          <CardTitle className="place-self-center text-4xl mt-3">Register user</CardTitle>
         </CardHeader>
-        <CardContent>
-          <form>
+        <CardContent className="m-5">
+          <form className="mt-5">
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="username">Username</Label>
-                <Input id="username" placeholder="Username" />
+                <Input id="username" placeholder="Username" className="bg-[#d9d9d9]" />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="Password" />
+                <Label htmlFor="password" className="mt-5">Password</Label>
+                <Input id="password" type="password" placeholder="Password" className="bg-[#d9d9d9]" />
               </div>
             </div>
           </form>
         </CardContent>
-        <CardFooter className="flex flex-col items-center">
-          <p className="mt-2 text-sm text-gray-600">
+        <CardFooter className="flex flex-col items-center m-5">
+          <p className="text-sm text-gray-600">
             Already registered?{" "}
-            <Link to="/login" className="text-blue-500">
+            <Link to="/login" className="text-white">
               Log in
             </Link>
           </p>
-          <div className="mt-4 flex w-full justify-between">
+          <div className="mt-4 flex w-4/5 justify-between">
             <Button variant="outline">Cancel</Button>
             <Button>Register</Button>
           </div>
