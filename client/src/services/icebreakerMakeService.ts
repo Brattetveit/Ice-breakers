7c2d1a76e877883a1ea7d9e15fee227d1a42aa39
Artemis Aarø <akaaroe@gmail.com>
2024-02-22 11:03:15 +0100

    icebreaker form now conected to server for icebreaker creation

---
+++ client/src/services/icebreakerMakeService.ts
@@ -0,0 +1,38 @@
+
+
+export const handleCreateIcebreaker =  async (
+    user: string,
+    nameText: string,
+    ruleText: string, 
+    summaryText: string,
+    category: string,
+    visibility: boolean
+  ) => {
+    const icebreaker = {
+      author: user,
+      name: nameText,
+      fullDescription: ruleText,
+      shortDescription: summaryText,
+      category: category,
+      visibility: visibility
+    };
+
+    try {
+      const response = await fetch("api/icebreakers/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(icebreaker)
+      });
+
+      if (response.ok) {
+        const NewIcebreaker = await response.json();
+        console.log("Icebreaker created", NewIcebreaker);
+      } else {
+        console.log("Icebreaker creation faild", response.statusText); 
+      }
+    } catch (error) {
+      console.error("error submiting new Icebreaker data", error);
+    }
+  };
