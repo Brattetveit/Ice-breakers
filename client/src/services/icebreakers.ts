7f921423915e83bc06ef835c9a281a043d5a3850
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-03-04 12:49:32 +0100

    update rating service

--- client/src/services/icebreakers.ts
+++ client/src/services/icebreakers.ts
@@ -3,3 +3,12 @@ export const fetchIcebreakers = async () => {
   const { data } = await response.json();
   return data;
 };
+
+export const updateRating = async (name: string, rating: number) => {
+  const response = await fetch("/api/icebreakers/rating/" + name, {
+    method: "POST",
+    body: JSON.stringify({ rating }),
+  });
+  const data = await response.json();
+  return data;
+};
