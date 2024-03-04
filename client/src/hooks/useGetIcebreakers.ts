158612cd49f2e756470d2e04ed78fb411dbc8a37
Jakob Tøssebro <jakob.tossebro@ntnu.no>
2024-03-04 14:05:47 +0100

    working filter

--- client/src/hooks/useGetIcebreakers.ts
+++ client/src/hooks/useGetIcebreakers.ts
@@ -6,11 +6,13 @@ export const useGetIcebreakers = () => {
   const [icebreakers, setIcebreakers] = useState<Icebreaker[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const getIcebreakers = (filter: boolean = true) => {
+  const getIcebreakers = (
+    query: (icebreaker: Icebreaker) => boolean = () => true,
+  ) => {
     setIsLoading(true);
 
     fetchIcebreakers()
-      .then((array: Icebreaker[]) => array.filter(() => filter))
+      .then((array: Icebreaker[]) => array.filter(query))
       .then((array) => array.sort(() => Math.random() - 0.5))
       .then(setIcebreakers)
       .then(() => setIsLoading(false));
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -14,7 +14,11 @@ export const Profile = () => {
   const { icebreakers, getIcebreakers } = useGetIcebreakers();
 
   useEffect(() => {
-    getIcebreakers(user);
+    getIcebreakers((icebreaker) => {
+      if (!user || !icebreaker.author) return false;
+
+      return icebreaker.author.username === user.username;
+    });
     setAuthored(icebreakers);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
