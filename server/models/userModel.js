83c79a59c3cba1ff30ef3347d8d6d2c607cacb05
Adrian Dahlen Haugen <adriandh@stud.ntnu.no>
2024-03-11 09:57:51 +0100

    feat: update userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Icebreakers" }],
+  icebreakerQueue: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Icebreakers" },
+  ],
+  createdIcebreakers: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Icebreakers" },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
