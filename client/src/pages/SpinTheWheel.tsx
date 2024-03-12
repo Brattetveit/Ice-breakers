0c9b3d968eaece9250dc3f7a4682c0b07f41315e
Athina Thayananthan <athinat@stud.ntnu.no>
2024-03-12 11:42:08 +0100

    implemented styling

--- client/src/pages/SpinTheWheel.tsx
+++ client/src/pages/SpinTheWheel.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useGetIcebreakers } from "@/hooks/useGetIcebreakers";
-import "./SpinTheWheel.css";
+import "./SpinTheWheel.css"; // Husk å oppdatere din CSS-fil basert på veiledningen gitt
 import { Icebreaker } from "@/types";
 
 const SpinTheWheel: React.FC = () => {
@@ -11,36 +11,41 @@ const SpinTheWheel: React.FC = () => {
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
   const [options, setOptions] = useState<string[]>([]);
   const [displayOption, setDisplayOption] = useState("Spinn");
-  const [filteredIcebreakers, setFilteredIcebreakers] = useState<Icebreaker[]>([]);
+  const [filteredIcebreakers, setFilteredIcebreakers] = useState<Icebreaker[]>(
+    [],
+  );
   const [showWarning, setShowWarning] = useState(false);
 
-  
-
   useEffect(() => {
-    getIcebreakers(); // Rettet fra useGetIcebreakers();
+    getIcebreakers();
   }, [getIcebreakers]);
 
   useEffect(() => {
-    const newFilteredIcebreakers = icebreakers.filter((icebreaker) =>
-      icebreaker.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase()),
-    ).sort((a, b) => a.name.localeCompare(b.name)); // Sorterer alfabetisk
-  
+    const newFilteredIcebreakers = icebreakers
+      .filter((icebreaker) =>
+        icebreaker.name
+          .toLowerCase()
+          .includes(debouncedSearchTerm.toLowerCase()),
+      )
+      .sort((a, b) => a.name.localeCompare(b.name));
+
     setFilteredIcebreakers(newFilteredIcebreakers);
   }, [debouncedSearchTerm, icebreakers]);
-  
 
   const addOption = (optionName: string) => {
     if (result !== "") {
-      console.log("Kan ikke legge til flere icebreakers etter at et resultat er generert.");
+      console.log(
+        "Kan ikke legge til flere icebreakers etter at et resultat er generert.",
+      );
       return;
     }
     if (options.includes(optionName)) {
-      setShowWarning(true); // Vis advarsel
-      setTimeout(() => setShowWarning(false), 3000); // Skjul advarselen automatisk etter 3 sekunder
+      setShowWarning(true);
+      setTimeout(() => setShowWarning(false), 3000);
     } else {
       setOptions([...options, optionName]);
-      setSearchTerm(""); // Tømmer søkefeltet og lukker rullegardinen
-      setShowWarning(false); // Sørg for at advarselen ikke vises hvis alt er ok
+      setSearchTerm("");
+      setShowWarning(false);
     }
   };
 
@@ -72,63 +77,136 @@ const SpinTheWheel: React.FC = () => {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1 className="tittel">Spinn hjulet!</h1>
-      <div style={{ width: "550px", margin: "auto" }}>
+    <div
+      className="page-container"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <div
+        className="banner-title"
+        style={{
+          width: "100%",
+          backgroundColor: "#c9e1fa",
+          padding: "20px 0",
+          boxSizing: "border-box",
+          fontSize: "70px",
+          fontFamily: '"ZCOOL XiaoWei"',
+          color: "rgb(0, 0, 0)",
+          textAlign: "center",
+        }}
+      >
+        Spinn hjulet!
+      </div>
+      <div className="content-container">
         <div
-          style={{
-            width: "550px",
-            height: "550px",
-            border: "10px solid #000",
-            borderRadius: "50%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "90px",
-            fontWeight: "bold",
-            cursor: "pointer",
-            background: result ? "#4CAF50" : spinning ? "#ccc" : "#fff",
-            transition: "background-color 0.5s ease",
-          }}
-          onClick={spinWheel}
+          style={{ display: "flex", flex: 1, height: "calc(100vh - 140px)" }}
         >
-          {result || displayOption}
+          {" "}
+          {/* Juster høyden basert på bannerets faktiske høyde */}
+          <div
+            className="wheel-container"
+            style={{
+              flexBasis: "50%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: "#edf2fb",
+              height: "100%",
+              flex: 1,
+            }}
+          >
+            {/* Hjulet her */}
+            <div
+              style={{
+                width: "500px",
+                height: "500px",
+                border: "10px solid #000",
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "90px",
+                fontWeight: "bold",
+                fontFamily: "ZCOOL XiaoWei",
+                cursor: "pointer",
+                background: result ? "#99e2b4" : spinning ? "#c9e1fa" : "#fff",
+                transition: "background-color 0.5s ease",
+              }}
+              onClick={spinWheel}
+            >
+              {result || displayOption}
+            </div>
+          </div>
+          <div
+            className="options-container"
+            style={{
+              flexBasis: "50%",
+              background: "#fadde1",
+              padding: "20px",
+              position: "relative",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* Søkefunksjon og alternativer her */}
+            {showWarning && (
+              <p style={{ color: "red" }}>
+                Dette alternativet er allerede lagt til.
+              </p>
+            )}
+            <div style={{ position: "relative", width: "100%" }}>
+              <input
+                className="inp"
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Søk etter icebreakers"
+                style={{ width: "50%", marginBottom: "10px", display: "flex" }}
+              />
+              {searchTerm && (
+                <ul
+                  className="search-results"
+                  style={{
+                    position: "absolute",
+                    width: "100%",
+                    top: "100%",
+                    left: 0,
+                    zIndex: 1,
+                    background: "white",
+                    borderTop: "none",
+                  }}
+                >
+                  {filteredIcebreakers.map((icebreaker: Icebreaker) => (
+                    <li
+                      key={icebreaker._id}
+                      onClick={() => addOption(icebreaker.name)}
+                    >
+                      {icebreaker.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <div>
+              <h2 className="alt" style={{ width: "100 %" }}>
+                Valgte alternativer:
+              </h2>
+              <ul className="alternativene" style={{ width: "100%" }}>
+                {options.map((option, index) => (
+                  <li key={index}>{option}</li>
+                ))}
+              </ul>
+            </div>
+            <button
+              className="reset-button"
+              onClick={resetOptions}
+              style={{ marginTop: "20px" }}
+            >
+              Tilbakestill liste
+            </button>
+          </div>
         </div>
       </div>
-      {result && <p>Resultat: {result}</p>}
-
-      <div>
-  {showWarning && <p style={{ color: 'red' }}>Dette alternativet er allerede lagt til.</p>}
-  <input
-    className="inp"
-    type="text"
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    placeholder="Søk etter icebreakers"
-  />
-  {searchTerm && (
-    <ul className="search-results">
-      {filteredIcebreakers.map((icebreaker: Icebreaker) => (
-        <li key={icebreaker._id} onClick={() => addOption(icebreaker.name)}>
-          {icebreaker.name}
-        </li>
-      ))}
-    </ul>
-  )}
-</div>
-
-      <div>
-        <h2 className="alt">Valgte alternativer:</h2>
-        <ul className="alternativene">
-          {options.map((option, index) => (
-            <li key={index}>{option}</li>
-          ))}
-        </ul>
-      </div>
-
-      <button className="reset-button" onClick={resetOptions}>
-        Tilbakestill liste
-      </button>
     </div>
   );
 };
@@ -148,8 +226,5 @@ function useDebounce(value: string, delay: number): string {
 
   return debouncedValue;
 }
-
-
-
 
 export default SpinTheWheel;
