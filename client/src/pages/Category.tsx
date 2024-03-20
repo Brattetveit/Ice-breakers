53034c34a1a507bac271c20e2926c82b40bcfb82
Olav Brattetveit <olavhbr@stud.ntnu.no>
2024-03-20 18:10:28 +0100

    Possible solution to category display

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -1,27 +1,78 @@
+// import "./Category.css";
+// import { Link } from "react-router-dom";
+// import { useIcebreakers } from "@/hooks/useIcebreakers";
+// import { useLocation, type Location } from "react-router-dom";
+// import { Category } from "@/types";
+// import { IcebreakerCard } from "@/components/IcebreakerCard";
+// export function InCategory() {
+//   const { icebreakers, isLoading } = useIcebreakers();
+
+//   const location: Location<{ category: Category }> = useLocation();
+//   const { category } = location.state;
+
+//   const getIcebreakersByCategory = (category: Category) => {
+//     return icebreakers.filter((icebreaker) => icebreaker.category === category);
+//   };
+
+
+//   return (
+//     <div className="category-page">
+//       <h1 className="category-title">{category}</h1>
+//       {isLoading ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <div className="icebreaker-list">
+//           {getIcebreakersByCategory(category).map((icebreaker, index) => (
+//             <IcebreakerCard key={index} icebreaker={icebreaker} />
+//           ))}
+//         </div>
+//       )}
+//       <Link
+//         to="/categoryPage"
+//         className="Tilbake"
+//         style={{
+//           backgroundColor: "#EEC5F8",
+//           color: "black",
+//           fontSize: "30px",
+//         }}
+//       >
+//         &#8249;
+//       </Link>
+//     </div>
+//   );
+// }
+
 import "./Category.css";
 import { Link } from "react-router-dom";
 import { useIcebreakers } from "@/hooks/useIcebreakers";
 import { useLocation, type Location } from "react-router-dom";
 import { Category } from "@/types";
 import { IcebreakerCard } from "@/components/IcebreakerCard";
+import { useEffect } from "react";
+
 export function InCategory() {
-  const { icebreakers } = useIcebreakers();
+  const { icebreakers, isLoading, getIcebreakersByCategory } = useIcebreakers();
 
   const location: Location<{ category: Category }> = useLocation();
   const { category } = location.state;
 
-  const getIcebreakersByCategory = (category: Category) => {
-    return icebreakers.filter((icebreaker) => icebreaker.category === category);
-  };
+  useEffect(() => {
+    getIcebreakersByCategory(category);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
 
   return (
     <div className="category-page">
       <h1 className="category-title">{category}</h1>
-      <div className="icebreaker-list">
-        {getIcebreakersByCategory(category).map((icebreaker, index) => (
-          <IcebreakerCard key={index} icebreaker={icebreaker} />
-        ))}
-      </div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="icebreaker-list">
+          {icebreakers.map((icebreaker, index) => (
+            <IcebreakerCard key={index} icebreaker={icebreaker} />
+          ))}
+        </div>
+      )}
       <Link
         to="/categoryPage"
         className="Tilbake"
@@ -36,3 +87,4 @@ export function InCategory() {
     </div>
   );
 }
+
