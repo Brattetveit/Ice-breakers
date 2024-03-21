36d2c65123b7f12d26f9e588e2e5ba35c6ffae67
Adrian Dahlen Haugen <adrianhaugen99@gmail.com>
2024-03-21 08:41:58 +0100

    fix category

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -1,24 +1,25 @@
 import "./Category.css";
 import { Link } from "react-router-dom";
 import { useIcebreakers } from "@/hooks/useIcebreakers";
+import { useEffect } from "react";
 import { useLocation, type Location } from "react-router-dom";
 import { Category } from "@/types";
 import { IcebreakerCard } from "@/components/IcebreakerCard";
 export function InCategory() {
-  const { icebreakers } = useIcebreakers();
+  const { icebreakers, getIcebreakersByCategory } = useIcebreakers();
 
   const location: Location<{ category: Category }> = useLocation();
   const { category } = location.state;
 
-  const getIcebreakersByCategory = (category: Category) => {
-    return icebreakers.filter((icebreaker) => icebreaker.category === category);
-  };
+  useEffect(() => {
+    getIcebreakersByCategory(category);
+  }, [category, getIcebreakersByCategory]);
 
   return (
     <div className="category-page">
       <h1 className="category-title">{category}</h1>
       <div className="icebreaker-list">
-        {getIcebreakersByCategory(category).map((icebreaker, index) => (
+        {icebreakers.map((icebreaker, index) => (
           <IcebreakerCard key={index} icebreaker={icebreaker} />
         ))}
       </div>
