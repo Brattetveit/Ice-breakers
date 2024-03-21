f2cbaba164c0119b263b7f143fc898ff56ad9d74
Olav Brattetveit <olavhbr@stud.ntnu.no>
2024-03-21 09:03:45 +0100

    category page copied from main

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -1,19 +1,62 @@
+import "./Category.css";
+import { Link } from "react-router-dom";
+import { useIcebreakers } from "@/hooks/useIcebreakers";
+import { useEffect } from "react";
+import { useLocation, type Location } from "react-router-dom";
+import { Category } from "@/types";
+import { IcebreakerCard } from "@/components/IcebreakerCard";
+export function InCategory() {
+  const { icebreakers, getIcebreakersByCategory } = useIcebreakers();
+
+  const location: Location<{ category: Category }> = useLocation();
+  const { category } = location.state;
+
+  useEffect(() => {
+    getIcebreakersByCategory(category);
+  }, [category, getIcebreakersByCategory]);
+
+  return (
+    <div className="category-page">
+      <h1 className="category-title">{category}</h1>
+      <div className="icebreaker-list">
+        {icebreakers.map((icebreaker, index) => (
+          <IcebreakerCard key={index} icebreaker={icebreaker} />
+        ))}
+      </div>
+      <Link
+        to="/categoryPage"
+        className="Tilbake"
+        style={{
+          backgroundColor: "#EEC5F8",
+          color: "black",
+          fontSize: "30px",
+        }}
+      >
+        &#8249;
+      </Link>
+    </div>
+  );
+}
+
+
 // import "./Category.css";
 // import { Link } from "react-router-dom";
 // import { useIcebreakers } from "@/hooks/useIcebreakers";
 // import { useLocation, type Location } from "react-router-dom";
 // import { Category } from "@/types";
 // import { IcebreakerCard } from "@/components/IcebreakerCard";
+// import { useEffect } from "react";
+
 // export function InCategory() {
-//   const { icebreakers, isLoading } = useIcebreakers();
+//   const { icebreakers, isLoading, getIcebreakersByCategory } = useIcebreakers();
 
 //   const location: Location<{ category: Category }> = useLocation();
 //   const { category } = location.state;
 
-//   const getIcebreakersByCategory = (category: Category) => {
-//     return icebreakers.filter((icebreaker) => icebreaker.category === category);
-//   };
-
+//   useEffect(() => {
+//     getIcebreakersByCategory(category);
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [category]);
 
 //   return (
 //     <div className="category-page">
@@ -22,7 +65,7 @@
 //         <div>Loading...</div>
 //       ) : (
 //         <div className="icebreaker-list">
-//           {getIcebreakersByCategory(category).map((icebreaker, index) => (
+//           {icebreakers.map((icebreaker, index) => (
 //             <IcebreakerCard key={index} icebreaker={icebreaker} />
 //           ))}
 //         </div>
@@ -41,50 +84,4 @@
 //     </div>
 //   );
 // }
-
-import "./Category.css";
-import { Link } from "react-router-dom";
-import { useIcebreakers } from "@/hooks/useIcebreakers";
-import { useLocation, type Location } from "react-router-dom";
-import { Category } from "@/types";
-import { IcebreakerCard } from "@/components/IcebreakerCard";
-import { useEffect } from "react";
-
-export function InCategory() {
-  const { icebreakers, isLoading, getIcebreakersByCategory } = useIcebreakers();
-
-  const location: Location<{ category: Category }> = useLocation();
-  const { category } = location.state;
-
-  useEffect(() => {
-    getIcebreakersByCategory(category);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category]);
-
-  return (
-    <div className="category-page">
-      <h1 className="category-title">{category}</h1>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="icebreaker-list">
-          {icebreakers.map((icebreaker, index) => (
-            <IcebreakerCard key={index} icebreaker={icebreaker} />
-          ))}
-        </div>
-      )}
-      <Link
-        to="/categoryPage"
-        className="Tilbake"
-        style={{
-          backgroundColor: "#EEC5F8",
-          color: "black",
-          fontSize: "30px",
-        }}
-      >
-        &#8249;
-      </Link>
-    </div>
-  );
-}
 
